cbbf095fc6f065ca87449d0d11770942a71f00ac
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2021-07-10 10:13:07 -0500

    made corrections

--- Assignment2/guessnum.java
+++ Assignment2/guessnum.java
@@ -7,28 +7,32 @@ public class guessnum{
       int random_num = num.nextInt(100) + 1;
       
       //start of scanner
-      Scanner scanner = new Scanner(System.in);
-      System.out.print("Pick a number between 1 and 100: ");
-      int guess = scanner.nextInt();
+     // Scanner scanner = new Scanner(System.in);
+     // System.out.print("Pick a number between 1 and 100: ");
+     // int guess = scanner.nextInt();
       
       //int i
-      int i = 0;
-      while(i < 4){
+      int guessCounter = 0;
+      while(guessCounter < 5){
+         Scanner scanner = new Scanner(System.in);
+         System.out.print("Pick a number between 1 and 100: ");
+         int guess = scanner.nextInt();
+
          while(guess >100 || guess < 1){
             System.out.print("Your guess is not between 1 and 100, please try again: ");
             guess = scanner.nextInt();
          }
           if(guess > random_num){
           System.out.print("Please pick a lower number: ");
-          guess = scanner.nextInt();
+          //guess = scanner.nextInt();
          }else if(guess < random_num){
           System.out.print("Please pick a higher number: ");
-          guess = scanner.nextInt();
+          //guess = scanner.nextInt();
          }else if(guess == random_num){
           System.out.print("You Win!!");
           System.exit(0);
          }
-      i++;
+      guessCounter++;
       }//end of while
       
            
